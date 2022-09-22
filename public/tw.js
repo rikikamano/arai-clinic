5736194cdfe3996ec3ec6338890f8e2f581b6fe8
rikika <hikikomororinn@gmail.com>
2022-09-22 12:43:43 +0900

    aa

---
+++ public/tw.js
@@ -0,0 +1,21 @@
+function changeTwitterWidgetDesign() {
+    var $twitter_widget = jQuery("iframe.twitter-timeline");
+    var $twitter_widget_contents = $twitter_widget.contents();
+  
+    if (
+      $twitter_widget.length > 0 &&
+      $twitter_widget[0].contentWindow.document.body.innerHTML !== ""
+    ) {
+      $twitter_widget_contents
+        .find("head")
+        .append(
+          "<style>.SandboxRoot .timeline-Widget .timeline-Tweet-text{font-size:200px;line-height:200px}</style>>"
+        );
+    } else {
+      setTimeout(function () {
+        changeTwitterWidgetDesign();
+      }, 350);
+    }
+  }
+  
+  changeTwitterWidgetDesign();
